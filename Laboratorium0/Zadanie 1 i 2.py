36e92b72ed1d74abf7e8878d4f4af54d1bd74f9d
Paulina <w69771@student.wsiz.edu.pl>
2023-11-09 19:01:38 +0100

    Komentarz do zadań 1 i 2.

---
+++ Laboratorium0/Zadanie 1 i 2.py
@@ -0,0 +1,20 @@
+# Zad 1
+
+# 1+2 int (integer-reprezentuje liczby całkowite)
+# 1+4.5 float(reprezentuje liczby zmiennoprzecinkowe)
+# 3/2 float
+# 4/2 float
+# 3//2 int
+# -3//2 int
+# 11%2 int
+# 2**10 int
+# 8**(1/3) float
+# + to dodawanie, / to dzielenie, // to dzielenie całkowite, % to reszta z dzielenia, ** to potęgowanie
+
+# Zad 2
+
+# int(3.0)=3 Zamienia float na int
+# float (3)=3.0 Zamienia int na float
+# float ("3")= 3.0 Zamienia string na float
+# str(12.4)= "12.4" zamienia float na string
+# bool(0)=false Reprezentuje wartość logiczną
