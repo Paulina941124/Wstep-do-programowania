b07d16df1283b11498834e12930660f8fd4ca45e
Paulina <w69771@student.wsiz.edu.pl>
2023-11-10 10:31:59 +0100

    Zadania z laboratorium 1

---
+++ Laboratorium1/Zadanie 1 i 2.py
@@ -0,0 +1,19 @@
+# Zad1
+
+wiek = int(input("Podaj swój wiek "))
+if wiek < 4:
+    print("Wstęp darmowy")
+elif wiek > 18:
+    print("Bilet kosztuje 20zł.")
+else:
+    print("Bilet kosztuje 10 zł.")
+
+# Zad2
+
+litera = input("Podaj literę ")
+if litera.isupper():
+    print("To jest duża litera.")
+elif litera.islower():
+    print("To jest mała litera.")
+else:
+    print("To nie jest litera.")
