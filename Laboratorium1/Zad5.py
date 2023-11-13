45b0596049fd41a2aeb14715878ef2e110b74a15
Paulina <w69771@student.wsiz.edu.pl>
2023-11-13 21:26:41 +0100

    Zadania z laboratorium 1

---
+++ Laboratorium1/Zad5.py
@@ -0,0 +1,7 @@
+x = int(input("Podaj pierwszą liczbę "))
+y = int(input("Podaj drugą liczbę "))
+z = int(input("Podaj trzecią liczbę "))
+
+liczby = [x, y, z]
+liczby.sort()
+print("Posortowane liczby ", liczby)
