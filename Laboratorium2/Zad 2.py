8577b61ffd697066af3b62795496abbb358e576c
Paulina <w69771@student.wsiz.edu.pl>
2023-12-07 16:18:04 +0100

    Zadania Lab 2

--- Laboratorium2/Zad 2.py
+++ Laboratorium2/Zad 2.py
@@ -3,7 +3,8 @@ xp = -4
 xk = 4
 krok = 0.5
 
-# Wypisanie liczb z  zakresu z określonym krokiem
+# W funkcji range można używać jedynie liczb całkowitych, a krok jest floatem
+# Dlatego najpierw je może przez 2 a nastepnie dzielę przez 2
 
 for x in range(int(xp * 2), int((xk + krok) * 2), int(krok * 2)):
     x1 = x/2.0
