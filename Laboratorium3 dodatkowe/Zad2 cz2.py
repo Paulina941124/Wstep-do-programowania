94b538d1d1be988543f289137dc090ea508745cb
Paulina <w69771@student.wsiz.edu.pl>
2024-02-03 21:54:47 +0100

    Ukończone. Gotowe do sprawdzenia.

--- Laboratorium3 dodatkowe/Zad2 cz2.py
+++ Laboratorium3 dodatkowe/Zad2 cz2.py
@@ -1,12 +1,12 @@
-szer = 6
-wys = 5
+szerok = 6
+wysok = 5
 
 przeciwnicy = [(0, 1), (2, 3), (2, 4), (3, 4)]
 monety = [(1, 1), (2, 0), (3, 3), (5, 3)]
 rzeka_y = 2
 
-for y in range(wys):
-    for x in range(szer):
+for y in range(wysok):
+    for x in range(szerok):
         # ustawienie przeciwników
         if (x, y) in przeciwnicy:
             print('x', end=' ')
